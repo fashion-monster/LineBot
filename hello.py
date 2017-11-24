2652b50f9f39a1637c9260546fb61905c60f7490
Yuta <user@user-no-MacBook-Air.local>
2017-11-24 18:54:24 +0900

    add add

--- hello.py
+++ hello.py
@@ -81,7 +81,7 @@ def image_message(event):
     message_content = line_bot_api.get_message_content(msg_id)
     f_path = 'https://fashion-monster-pbl.herokuapp.com/app/'+msg_id+'.jpg'
     try:
-        with open(f_path, 'w') as fd:
+        with open(f_path, 'w+') as fd:
             for chunk in message_content.iter_content():
                 fd.write(chunk)
         line_bot_api.reply_message(
