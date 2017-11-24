5615abc0dcf736dcc179eb29910223779cf54cde
Yuta <user@user-no-MacBook-Air.local>
2017-11-24 18:58:47 +0900

    add

--- hello.py
+++ hello.py
@@ -79,14 +79,14 @@ def handle_follow(event):
 def image_message(event):
     msg_id = event.message.id
     message_content = line_bot_api.get_message_content(msg_id)
-    f_path = msg_id+'.jpg'
+    f_path ='/tmp/'+msg_id+'.jpg'
     try:
-        with open(f_path, 'w+') as fd:
+        with open(f_path, 'w') as fd:
             for chunk in message_content.iter_content():
                 fd.write(chunk)
         line_bot_api.reply_message(
             event.reply_token,
-            ImageSendMessage(original_content_url='https://fashion-monster-pbl.herokuapp.com/app/'+f_path, preview_image_url='https://fashion-monster-pbl.herokuapp.com/app/'+f_path)
+            ImageSendMessage(original_content_url='https://fashion-monster-pbl.herokuapp.com'+f_path, preview_image_url='https://fashion-monster-pbl.herokuapp.com'+f_path)
         )
     except:
         import traceback
