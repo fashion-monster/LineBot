df12ef04d4ab1a3bb10913ae1f91438fc8358238
hashimock <g031m134@s.iwate-pu.ac.jp>
2017-12-01 16:08:28 +0900

    refactor
    refactor

--- resize_app.py
+++ resize_app.py
@@ -1,22 +1,22 @@
 from flask import Flask, request
 from utils.reshape import reshape
 
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+import cv2
+import numpy as np
 app = Flask(__name__)
 
 
 @app.route("/resize", methods=['POST'])
-def main():
+def resize():
     """
     resize method.
     Read "image_name" key and resize it.
 
     :return: success or failed, cv2.imwrite
     """
-    import tensorflow as tf
-    import tensorflow.contrib.eager as tfe
-    import cv2
-    import numpy as np
-    tfe.enable_eager_execution()
+
     image_name = request.args.get("image_path")
     image = tf.constant(np.asarray(cv2.imread(filename=image_name)))
     cropped_image = reshape(image=image, new_size=None)
@@ -25,4 +25,5 @@ def main():
 
 
 if __name__ == '__main__':
+    tfe.enable_eager_execution()
     app.run(port=9999)
