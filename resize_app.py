9c2cda6525bf450ce049db979a7a89a84a669e3f
hashimock <g031m134@s.iwate-pu.ac.jp>
2017-12-01 15:42:06 +0900

    rename key

--- resize_app.py
+++ resize_app.py
@@ -17,7 +17,7 @@ def main():
     import cv2
     import numpy as np
     tfe.enable_eager_execution()
-    image_name = request.args.get("image_name")
+    image_name = request.args.get("image_path")
     image = tf.constant(np.asarray(cv2.imread(filename=image_name)))
     cropped_image = reshape(image=image, new_size=None)
     DIRECTORY='cropped/'
