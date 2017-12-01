cd658d55366aa2ff0c20fcdc3eda8463e8def1e0
yuguno <31914737+yuguno@users.noreply.github.com>
2017-12-02 03:05:49 +0900

    push tuika

--- hello.py
+++ hello.py
@@ -204,6 +204,7 @@ def confirm_message(event):
             ])
     elif text == 'テスト':
         line_bot_api.push_message('U68c89b1ff06c2a997c249340fae7040b', TextSendMessage(text='message1'))
+        line_bot_api.push_message('U4fce6cc2cc3530ae2f4b7ca0609edd40', TextSendMessage(text='message1'))
     elif text == '確認':
         test_text = event.source.user_id
         line_bot_api.reply_message(
