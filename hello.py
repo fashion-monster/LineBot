547e366c7a7bd806e145e4db9786a41e3de932e6
mottirioisii <mottirioisii@gmail.com>
2017-12-06 20:00:33 +0900

    remove signature

--- hello.py
+++ hello.py
@@ -61,17 +61,8 @@ def callback():
 
 @app.route("/get_suggestion", methods=['POST'])
 def get_suggestion():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # handle web-hook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
 
     return 'OK'
 
