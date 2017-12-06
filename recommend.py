bfd2d3d0ca503de298e0544a0ed4047fc0691479
Yuta <user@user-no-MacBook-Air.local>
2017-12-06 09:05:26 +0900

    とりあえず推薦できるようになったがpythonぽくないので後でリファクタする

--- recommend.py
+++ recommend.py
@@ -58,7 +58,9 @@ def get_best_clothes_by_rank(rank_list,self_rank):
         'Bottoms':[],
         'similarity':1
     }
-
+    # ここらの実装は要検討
+    # 多分組み合わせ問題を解く必要が出てくる
+    # tops 及び bottoms のランキング画像複数あるときどうしようね
     for row in rank_list:
         if rank[row['type']] == [] and row['similarity'] != 0:
             rank[row['type']] = [{
@@ -85,6 +87,7 @@ def get_best_clothes_by_rank(rank_list,self_rank):
         rank['similarity'] = rank['similarity']*row['similarity']
     return {self_rank : rank}
 
+
 with open('all_pattern_of_Similarity.csv', 'r') as f:
     reader = csv.reader(f)
     header = next(reader)
@@ -99,5 +102,20 @@ with open('all_pattern_of_Similarity.csv', 'r') as f:
     for row in sim_dict:
         most_sim.append(get_best_clothes_by_rank(sim_dict[row],row))
 
+    similarity_ranking = {}
+    most_sim_recommend_key = []
+    RECOMEND_MAX = 5
+    for k in most_sim:
+        for row in k:
+            similarity_ranking[str(row)] = k[str(row)]['similarity']
+    for k,v in sorted(similarity_ranking.items(), key=lambda x:x[1],reverse=True):
+        most_sim_recommend_key.append(k)
+    print most_sim_recommend_key
+    for k in most_sim:
+        for index in k:
+            for i in range(0,RECOMEND_MAX):
+                if index == most_sim_recommend_key[i]:
+                    # kに推薦したいオブジェクトが入ってるのでBot側で頑張って
+                    pprint(k)
 
-    pprint(most_sim)
+    
